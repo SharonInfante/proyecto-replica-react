5292dc640bbce88fe179df9db8e1e34463552a54
SharonInfante <sharonshabel@gmail.com>
2022-12-14 09:39:27 +0100

    modificado app.js

--- src/Js/App.js
+++ src/Js/App.js
@@ -1,18 +1,19 @@
 import Button from "../components/Button";
 import "../Css/index.css"
 import NavBar from "../components/NavBar";
-import Formulario from "../components/Formulario";
 
 function App() {
   return (
     <div className="Container">
       <header className="header">
         <>
-        <NavBar/>
+        <NavBar/>  
         </>
       </header>
       <main>
-        
+        <h1>Feel The Music</h1>
+        <p>Stream over 20 thousand songs with one click</p>
+        <Button/>
       </main>
     </div>
   );
